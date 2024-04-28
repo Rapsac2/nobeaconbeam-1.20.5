cb0c42d4afdb214e8ff2480a2f7999722e6ea838
Rapsac <caspar.kersting@gmail.com>
2024-04-28 17:19:25 +0200

    final oder so

--- src/main/java/net/rapsac/NoBeaconBeam.java
+++ src/main/java/net/rapsac/NoBeaconBeam.java
@@ -3,15 +3,32 @@ package net.rapsac;
 import net.fabricmc.api.ClientModInitializer;
 
 
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+import org.lwjgl.glfw.GLFW;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class NoBeaconBeam implements ClientModInitializer {
 	public static final String MOD_ID = "nobeaconbeam";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+	public static Boolean shouldRenderBeamBool = false;
 
 	@Override
 	public void onInitializeClient() {
-		LOGGER.info("Hello Fabric world!");
+		//LOGGER.info("Hello Fabric world!");
+		KeyBinding RenderCpsBeam = KeyBindingHelper.registerKeyBinding(new KeyBinding("toggle Beam", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_UNKNOWN, "NoBeaconBeam"));
+
+		ClientTickEvents.END_CLIENT_TICK.register(client -> {
+			if (RenderCpsBeam.wasPressed()){
+				if(shouldRenderBeamBool){
+					shouldRenderBeamBool = false;
+				}else{
+					shouldRenderBeamBool = true;
+				}
+			}
+		});
 	}
 }
--- src/main/java/net/rapsac/mixin/BeaconMixin.java
+++ src/main/java/net/rapsac/mixin/BeaconMixin.java
@@ -1,15 +1,19 @@
 package net.rapsac.mixin;
 
+import net.minecraft.client.render.block.entity.BeaconBlockEntityRenderer;
 import net.minecraft.server.MinecraftServer;
+import net.rapsac.NoBeaconBeam;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(MinecraftServer.class)
+@Mixin(BeaconBlockEntityRenderer.class)
 public class BeaconMixin {
-	@Inject(at = @At("HEAD"), method = "loadWorld")
-	private void init(CallbackInfo info) {
-		// This code is injected into the start of MinecraftServer.loadWorld()V
+	@Inject(at = @At("HEAD"), method = "renderBeam(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;Lnet/minecraft/util/Identifier;FFJII[FFF)V", cancellable = true)
+	private static void MixinMethode(CallbackInfo ci) {
+		if(!NoBeaconBeam.shouldRenderBeamBool){
+			ci.cancel();
+		}
 	}
 }
